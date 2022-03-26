0e5bb3bbc4a268f58dc1083a353497dea86c091b
A. Kupreishvili <92047667+hexley21@users.noreply.github.com>
2022-03-27 02:20:11 +0400

    Add sudoku validator script

---
+++ sudoku_validator.py
@@ -0,0 +1,28 @@
+def valid_solution(board):
+    # Gets rows from the board
+    rows = [set(x) for x in board]
+    # Gets columns from the board
+    columns = [set([j[i] for j in board]) for i in range(len(board))]
+    # Gets blocks fom the board
+    blocks = [[],[],[],[],[],[],[],[],[]]
+    x = 0; z = 0
+    for i in range(9):
+        for j in range(z, 3 + z):
+            for k in range(x, 3 + x):
+                blocks[i].append(board[j][k])
+        x += 3
+        if(x == 9): 
+          x = 0; z += 3
+
+    # Validates rows, columns and blocks
+    for i in range(9):
+        if {1,2,3,4,5,6,7,8,9} != rows[i]:
+            print(f"#{i} row is invalid")
+            return False
+        if {1,2,3,4,5,6,7,8,9} != columns[j]: 
+            print(f"#{i} column is invalid")
+            return False
+        if {1,2,3,4,5,6,7,8,9} != set(blocks[k]):
+            print(f"#{i} block is invalid")
+            return False
+    return True
